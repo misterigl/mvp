3a4135926fb67482a71ef428f1139e23de44b38e
Michael Ilg <michael@ilg.ac>
2016-12-05 14:24:23 -0800

    Integrate mongoDB and mongoose

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 var express = require('express')
+var mongoose = require('mongoose');
+
+
 var app = express()
 
 app.use(express.static('client'))
@@ -7,3 +10,13 @@ app.use('/lib', express.static('node_modules'))
 app.listen(3000, function () {
   console.log('ETH DApps listening on port 3000!')
 })
+
+
+mongoose.connect('mongodb://localhost/ethdapps');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('we are connected to Mongo!');
+});
+
