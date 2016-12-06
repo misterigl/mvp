67db749c9983be50b4dcecacbfc39bb4d80c2a9d
Michael Ilg <michael@ilg.ac>
2016-12-06 12:34:20 -0800

    Add click function to find tweets

--- client/app/app.js
+++ client/app/app.js
@@ -4,6 +4,11 @@ let ethDapp = angular.module('ethDapp', ['ngMaterial']);
 ethDapp.controller('EthListController', function EthListController($scope, $http) {
   $scope.dappList = [];
   $scope.tweetList = [1,2,3,4];
+
+  $scope.findTweets = (name) => {
+    console.log('got clicked with ', name);
+  }
+
   $http({
   method: 'GET',
   url: '/ethdapps',
