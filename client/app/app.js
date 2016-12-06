c72ebd2077b76659b1feeacb788f7d6e2191746f
Michael Ilg <michael@ilg.ac>
2016-12-06 12:14:26 -0800

    Add list for tweets with example data

--- client/app/app.js
+++ client/app/app.js
@@ -3,6 +3,7 @@ let ethDapp = angular.module('ethDapp', ['ngMaterial']);
 
 ethDapp.controller('EthListController', function EthListController($scope, $http) {
   $scope.dappList = [];
+  $scope.tweetList = [1,2,3,4];
   $http({
   method: 'GET',
   url: '/ethdapps',
